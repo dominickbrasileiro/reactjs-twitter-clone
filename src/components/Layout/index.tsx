42a72841abdf113f118a9044cb67fdcc30df32dc
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-01 05:54:19 -0300

    Add Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import Main from '../Main';
+
+import { Container, Wrapper } from './styles';
+
+const Layouts: React.FC = () => (
+  <Container>
+    <Wrapper>
+      {/* <MenuBar /> */}
+      {/* <Main /> */}
+      {/* <SideBar /> */}
+    </Wrapper>
+  </Container>
+);
+
+export default Layouts;
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: var(--primary);
+`;
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  max-width: 1280px;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+`;
