fb716635fe5295ca51d3db9d44eaac455273f74b
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-01 05:26:28 -0300

    Add GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <h1>Rocketseat</h1>
+    <>
+      <h1>Rocketseat</h1>
+
+      <GlobalStyles />
+    </>
   );
 }
 
