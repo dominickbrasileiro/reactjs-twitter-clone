6e465a904f674c55bcbe866d5e80c3141263ed82
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-02 04:21:29 -0300

    Add ProfilePage component

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+} from './styles';
+
+const ProfilePage: React.FC = () => (
+  <Container>
+    <Banner>
+      <Avatar />
+    </Banner>
+
+    <ProfileData>
+      {/* <EditButton outlined>Editar perfil</EditButton> */}
+
+      <h1>Dominick Brasileiro</h1>
+      <h2>@dominickjs</h2>
+
+      <p>
+        Software Developer | Fascinated by Technology
+      </p>
+
+      <ul>
+        <li>
+          <LocationIcon />
+          Joinville, Brazil
+        </li>
+
+        <li>
+          <CakeIcon />
+          Born August 26, 2003
+        </li>
+      </ul>
+
+      <Followage>
+        <span>
+          <strong>501</strong>
+          {' '}
+          Following
+        </span>
+
+        <span>
+          <strong>1.3K</strong>
+          {' '}
+          Followers
+        </span>
+      </Followage>
+    </ProfileData>
+  </Container>
+);
+
+export default ProfilePage;
